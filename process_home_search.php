f415adb4c604ca5121558d0bbd8a9751b69318e9
Dinesh Kumar <mapmydestination@gmail.com>
2024-10-05 22:44:01 +0530

    test

--- process_home_search.php
+++ process_home_search.php
@@ -84,9 +84,9 @@ input[type=text]:focus{ color:#fff;}
 /*include_once ('database_connection.php');*/
 
 DEFINE('DATABASE_USER', 'root');
-DEFINE('DATABASE_PASSWORD','root');
+DEFINE('DATABASE_PASSWORD','');
 DEFINE('DATABASE_HOST', 'localhost');
-DEFINE('DATABASE_NAME', 'jobportal');
+DEFINE('DATABASE_NAME', 'jobcastlleportal');
 
 
 $dbc = @mysqli_connect(DATABASE_HOST, DATABASE_USER, DATABASE_PASSWORD,DATABASE_NAME);
